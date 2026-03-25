563671d5285b6776c7df1c6983bb281ffa6baa99
mohamedtareq24 <1900889@eng.asu.edu.eg>
2026-03-25 18:31:06 +0200

    added preload logic

--- src/clock_lane_defs.svh
+++ src/clock_lane_defs.svh
@@ -1,7 +1,7 @@
 `define TX_ESC_CLKPER   50      //50 ns TxClkEsc period (20 MHz)
 `define TX_LPX          1*`TX_ESC_CLKPER     // 1 Lpx time
 `define TX_INIT_TIME    1000
-`define LP_CLK_STOP_TIME 1000
+`define LP_CLK_STOP_TIME 100
 `define LP_CLK_REQ_TIME  1000
 `define LP_CLK_PRPR_TIME 1000
 `define LP_CLK_ZERO_TIME 1000
